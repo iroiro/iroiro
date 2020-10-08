f0a3bf410745ca75a5736d23af98e94eb8ae2d35
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-08 16:43:24 +0900

    Format component

--- frontend/packages/react-app/src/App.js
+++ frontend/packages/react-app/src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
-import {Contract} from "@ethersproject/contracts";
-import {getDefaultProvider, Web3Provider} from "@ethersproject/providers";
-import {abis, addresses} from "@project/contracts";
 import TopPage from "./components/pages/TopPage";
 
 const App = () => (
   // pages should be routed with router
-  <TopPage />
+  <TopPage/>
 )
 
 export default App;
