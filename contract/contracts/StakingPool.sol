196a197585fee9631d0488fa65f2072a10b9ce8b
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-09 17:35:07 +0900

    Add registering token to staking list

--- contract/contracts/StakingPool.sol
+++ contract/contracts/StakingPool.sol
@@ -1,14 +1,25 @@
 pragma solidity ^0.6.0;
 
 import "./interfaces.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract StakingPool is PoolInterface {
+// Owner must be TokenFactory
+contract StakingPool is PoolInterface, Ownable {
+    using SafeMath for uint256;
+
+    mapping(address => bool) public registeredTokens;
+
+    function addTokenToStakingList(address token) public onlyOwner {
+        registeredTokens[token] = true;
+    }
+
     function earned(address account, address token) public override view returns(uint256) {
         return 0;
     }
 
     function stake(uint256 amount, address token) public override {
-
+        require(registeredTokens[token], "Token is not registered to staking list");
     }
 
     function withdraw(uint256 amount, address token) public override {
--- contract/test/StakingPool.test.js
+++ contract/test/StakingPool.test.js
@@ -3,15 +3,56 @@ const {constants} = require("@openzeppelin/test-helpers")
 const {assert, expect} = require("chai")
 
 const StakingPool = contract.fromArtifact("StakingPool")
+const FanToken = contract.fromArtifact("FanToken")
 
 describe("StakingPool", () => {
   const [owner, alice, bob] = accounts;
 
   beforeEach(async () => {
     this.pool = await StakingPool.new({from: owner})
+    this.abctoken = await FanToken.new(
+      "ABCToken", "ABC", 1000000000, owner, 5, {from: owner}
+    )
+    this.xyztoken = await FanToken.new(
+      "XYZToken", "XYZ", 1000000000, owner, 5, {from: owner}
+    )
   })
 
-  it("return earned amount", async() => {
-    expect((await this.pool.earned(alice, constants.ZERO_ADDRESS)).toString()).to.equal("0")
+  describe("addTokenToStakingList", () => {
+    it("add token as staking token", async() => {
+      expect(await this.pool.registeredTokens(this.abctoken.address)).to.equal(false)
+      await this.pool.addTokenToStakingList(this.abctoken.address, { from: owner})
+      expect(await this.pool.registeredTokens(this.abctoken.address)).to.equal(true)
+    })
+
+    it("can not add token from non owner", async() => {
+      try {
+        await this.pool.addTokenToStakingList(this.abctoken.address, { from: alice})
+        assert.fail()
+      } catch(error) {
+        assert(true)
+      }
+    })
+  })
+
+  describe("earned", () => {
+    it("return earned amount", async () => {
+      expect((await this.pool.earned(alice, constants.ZERO_ADDRESS)).toString()).to.equal("0")
+    })
+  })
+
+  describe("stake", () => {
+    beforeEach(async() => {
+      await this.pool.addTokenToStakingList(this.abctoken.address, { from:owner })
+    })
+
+    it("cannot stake token not registered", async () => {
+      try {
+        await this.pool.stake(100, bob)
+        assert.fail("should throw an error")
+      } catch (error) {
+        assert(true)
+      }
+    })
   })
 })
