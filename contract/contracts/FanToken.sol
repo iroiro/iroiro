7c81866d8dbad9a00d0e4f138db6f09314b145fb
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-12 16:39:21 +0900

    Add minting with specific address

--- contract/contracts/FanToken.sol
+++ contract/contracts/FanToken.sol
@@ -3,14 +3,23 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FanToken is ERC20 {
+    address private _minter;
+
     constructor(
         string memory name,
         string memory symbol,
         uint256 totalSupply,
         address owner,
-        uint8 decimals
+        uint8 decimals,
+        address minter // Minter is going to be staking pool contract
     ) public ERC20(name, symbol) {
         _mint(owner, totalSupply);
         _setupDecimals(decimals);
+        _minter = minter;
+    }
+
+    function mint(address account, uint256 amount) public {
+        require(msg.sender == _minter, "Sender does not have minter role.");
+        _mint(account, amount);
     }
 }
--- contract/test/FanToken.test.js
+++ contract/test/FanToken.test.js
@@ -4,15 +4,15 @@ const {assert, expect} = require("chai")
 const FanToken = contract.fromArtifact("FanToken")
 
 describe("FanToken", () => {
-  const [owner, alice] = accounts;
+  const [owner, alice, minter] = accounts;
 
   beforeEach(async () => {
     this.fanStockToken = await FanToken.new(
-      "FanStock", "FST", 1000000000, owner, 10, {from: owner}
-      )
+      "FanStock", "FST", 1000000000, owner, 10, minter, {from: owner}
+    )
     this.aliceToken = await FanToken.new(
-      "AliceToken", "ALC", 2000000000, alice, 15, {from: owner}
-      )
+      "AliceToken", "ALC", 2000000000, alice, 15, minter, {from: owner}
+    )
   })
 
   it("has a name", async () => {
@@ -25,18 +25,32 @@ describe("FanToken", () => {
     expect(await this.aliceToken.symbol()).to.equal("ALC");
   })
 
-  it("mints a token", async() => {
+  it("mints a token", async () => {
     expect((await this.fanStockToken.totalSupply()).toString()).to.equal("1000000000")
     expect((await this.aliceToken.totalSupply()).toString()).to.equal("2000000000")
   })
 
-  it("transferred a token to creator", async() => {
+  it("transferred a token to creator", async () => {
     expect((await this.fanStockToken.balanceOf(owner)).toString()).to.equal("1000000000")
     expect((await this.aliceToken.balanceOf(alice)).toString()).to.equal("2000000000")
   })
 
-  it("set decimals given as argument", async() => {
+  it("set decimals given as argument", async () => {
     expect((await this.fanStockToken.decimals()).toString()).to.equal("10")
     expect((await this.aliceToken.decimals()).toString()).to.equal("15")
+  })
+
+  it("sender not a minter can not mint a token", async () => {
+    try {
+      await this.fanStockToken.mint(alice, 100, {from: alice})
+      assert.fail()
+    } catch (error) {
+      assert(true)
+    }
+  })
+
+  it("minter can mint a token", async () => {
+    await this.fanStockToken.mint(alice, 100, {from: minter})
+    expect((await this.fanStockToken.balanceOf(alice)).toString()).to.equal("100")
   })
 })
