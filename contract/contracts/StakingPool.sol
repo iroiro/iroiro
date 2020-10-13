7d0bf8fe88d3d27b94c439b7b7c3f7ede29c8157
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-14 08:24:35 +0900

    Add emitting events on StakingPool contract

--- contract/contracts/StakingPool.sol
+++ contract/contracts/StakingPool.sol
@@ -9,13 +9,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract StakingPool is PoolInterface, Ownable {
     using SafeMath for uint256;
 
-    // TODO define events
+    event Stake(
+        address indexed from,
+        address indexed token,
+        uint256 amount
+    );
 
-    mapping(address  => uint256) public tokensTotalSupply;
+    event Withdraw(
+        address indexed from,
+        address indexed token,
+        uint256 amount
+    );
+
+    mapping(address => uint256) public tokensTotalSupply;
     mapping(address => mapping(address => uint256)) private creatorsTokenBalances;
     mapping(address => bool) public registeredTokens;
 
-    function balanceOf(address account, address token) public view returns(uint256){
+    function balanceOf(address account, address token) public view returns (uint256){
         return creatorsTokenBalances[account][token];
     }
 
@@ -28,17 +38,20 @@ contract StakingPool is PoolInterface, Ownable {
         address token,
         uint256 totalSupply,
         uint8 decimals
-    ) public override view returns(uint256) {
+    ) public override view returns (uint256) {
         // TODO: claim amount is currently simplest mock. It will be replaced with formula
         return balanceOf(account, token).mul(10 ** uint256(decimals)).div(totalSupply);
     }
 
     function stake(uint256 amount, address token) public override {
         require(registeredTokens[token], "Token is not registered to staking list");
+
         creatorsTokenBalances[msg.sender][token] = creatorsTokenBalances[msg.sender][token].add(amount);
         tokensTotalSupply[token] = tokensTotalSupply[token].add(amount);
         FanToken fanToken = FanToken(token);
         fanToken.transferFrom(msg.sender, address(this), amount);
+
+        emit Stake(msg.sender, token, amount);
     }
 
     function withdraw(uint256 amount, address token) public override {
@@ -46,6 +59,8 @@ contract StakingPool is PoolInterface, Ownable {
         tokensTotalSupply[token] = tokensTotalSupply[token].sub(amount);
         FanToken fanToken = FanToken(token);
         fanToken.transfer(msg.sender, amount);
+
+        emit Withdraw(msg.sender, token, amount);
     }
 
     function claim(address token) public override {
