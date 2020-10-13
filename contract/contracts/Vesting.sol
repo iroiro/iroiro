c3735199a3c28e3b71106be3f6ad6a7d71980651
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-13 09:45:15 +0900

    Add adding vesting token

--- contract/contracts/Vesting.sol
+++ contract/contracts/Vesting.sol
@@ -1,7 +1,31 @@
 pragma solidity ^0.6.0;
 
-contract Vesting {
-    function redeem() public {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Vesting is Ownable {
+
+    mapping(address => bool) public vestingTokens;
+    mapping(address => uint256) public tokensVestingAmount;
+    mapping(address => address) public tokensRecipient;
+    mapping(address => uint256) public tokensVestingStart;
+    mapping(address => uint256) public tokensVestingEnd;
+
+    function addVesting(
+        address token,
+        address recipient,
+        uint256 vestingAmount,
+        uint256 vestingStart,
+        uint256 vestingEnd
+    ) public onlyOwner {
+        require(!vestingTokens[token], "Token is already registered");
+        vestingTokens[token] = true;
+        tokensVestingAmount[token] = vestingAmount;
+        tokensRecipient[token] = recipient;
+        tokensVestingStart[token] = vestingStart;
+        tokensVestingEnd[token] = vestingEnd;
+    }
+
+    function redeem(address token) public {
 
     }
 }
--- contract/test/Vesting.test.js
+++ contract/test/Vesting.test.js
@@ -6,19 +6,82 @@ const TokenFactory = contract.fromArtifact("TokenFactory")
 const Vesting = contract.fromArtifact("Vesting")
 
 describe("Vesting", () => {
-  const [owner] = accounts
+  const [owner, alice] = accounts
+  const totalSupply = 1000000000
+  const startTime = 1577836800 // Jan 1st 2020
+  const endTime = 1609459200 // Jan 1st 2021
 
-  describe("redeem", () => {
+  beforeEach(async () => {
+    this.vesting = await Vesting.new({from: owner})
+    this.abctoken = await FanToken.new(
+      "ABCToken", "ABC", totalSupply, owner, 5, {from: owner}
+    )
+    await this.abctoken.transfer(this.vesting.address, totalSupply * 50 / 100, {from: owner})
+    assert((await this.abctoken.balanceOf(this.vesting.address)).toString() === "500000000")
+  })
 
-    beforeEach(async () => {
-      this.vesting = await Vesting.new({from: owner})
+  describe("addVesting", () => {
+    it("register token as vesting", async () => {
+      expect(await this.vesting.vestingTokens(this.abctoken.address)).to.equal(false)
+      await this.vesting.addVesting(
+        this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: owner}
+      )
+      expect(await this.vesting.vestingTokens(this.abctoken.address)).to.equal(true)
     })
 
-    it("does not throw error", async() => {
+    it("throw error when sender is not a owner", async () => {
       try {
-        await this.vesting.redeem()
+        await this.vesting.addVesting(
+          this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: alice}
+        )
+        assert.fail()
+      } catch (error) {
         assert(true)
-      } catch(error) {
+      }
+    })
+
+    it("throw error when trying add token already registered", async () => {
+      await this.vesting.addVesting(
+        this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: owner}
+      )
+      try {
+        await this.vesting.addVesting(
+          this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: owner}
+        )
+        assert.fail()
+      } catch (error) {
+        assert(true)
+      }
+    })
+
+    it("increment token amount", async () => {
+      await this.vesting.addVesting(
+        this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: owner}
+      )
+      expect((await this.vesting.tokensVestingAmount(this.abctoken.address)).toString()).to.equal("500000000")
+    })
+
+    it("register account as recipient", async () => {
+      await this.vesting.addVesting(
+        this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: owner}
+      )
+      expect(await this.vesting.tokensRecipient(this.abctoken.address)).to.equal(alice)
+    })
+
+    it("register account as recipient", async () => {
+      await this.vesting.addVesting(
+        this.abctoken.address, alice, totalSupply * 50 / 100, startTime, endTime, {from: owner}
+      )
+      expect(await this.vesting.tokensRecipient(this.abctoken.address)).to.equal(alice)
+    })
+  })
+
+  describe("redeem", () => {
+    it("does not throw error", async () => {
+      try {
+        await this.vesting.redeem(this.abctoken.address)
+        assert(true)
+      } catch (error) {
         assert.fail("should not throw an error")
       }
     })
