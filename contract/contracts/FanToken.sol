7686f1dd49a782460a03641dc620b8851e84bf10
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-14 10:09:14 +0900

    Update comments

--- contract/contracts/FanToken.sol
+++ contract/contracts/FanToken.sol
@@ -4,8 +4,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract FanToken is ERC20 {
     address private minter;
-    uint8 public creatorTokenRatio; // fansTokenRatio = 100 - creatorTokenRatio
-    uint8 public lockupPeriod; // years
+    uint8 public creatorTokenRatio;
+    uint8 public lockupPeriod;
 
     constructor(
         string memory name,
@@ -14,8 +14,8 @@ contract FanToken is ERC20 {
         address owner,
         uint8 decimals,
         address _minter, // Minter is going to be staking pool contract
-        uint8 _creatorTokenRatio,
-        uint8 _lockupPeriod
+        uint8 _creatorTokenRatio, // fansTokenRatio = 100 - creatorTokenRatio
+        uint8 _lockupPeriod // years
     ) public ERC20(name, symbol) {
         _mint(owner, totalSupply);
         _setupDecimals(decimals);
