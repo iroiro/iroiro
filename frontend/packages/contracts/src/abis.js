7f276b8df2c994b3fd6491d54491b108a8852577
toshiakitakase <toshi@tart.tokyo>
2020-10-14 13:35:15 +0900

    Add redeem and stop staking function

--- frontend/packages/contracts/src/abis.js
+++ frontend/packages/contracts/src/abis.js
@@ -2,12 +2,16 @@ import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
 import tokenFactoryAbi from "./abis/TokenFactory.json";
 import fanTokenAbi from "./abis/FanToken.json";
+import stakingAbi from "./abis/Staking.json"
+import vestingAbi from "./abis/Vesting.json";
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
   tokenFactory: tokenFactoryAbi,
   fanToken: fanTokenAbi,
+  staking: stakingAbi,
+  vesting: vestingAbi,
 };
 
 export default abis;
--- frontend/packages/react-app/src/components/pages/DashboardPage.jsx
+++ frontend/packages/react-app/src/components/pages/DashboardPage.jsx
@@ -3,13 +3,10 @@ import {getDefaultProvider, Web3Provider} from "@ethersproject/providers";
 import {web3Modal} from "../../utils/web3Modal";
 import {Contract} from "@ethersproject/contracts";
 import {abis, addresses} from "@project/contracts";
-import Dashboard from "../templates/Dashboard"
+import Dashboard from "../templates/DashboardPageTemplate"
 
 const DashboardPage = () => {
   const [provider, setProvider] = useState();
-  const [path , setPath] = useState("");
-  const [name , setName] = useState("");
-  const [vestingAmount , setVestingAmount] = useState("");
   const [tokens , setTokensAddress] = useState([]);
   
   const loadWeb3Modal = useCallback(async () => {
@@ -19,7 +16,6 @@ const DashboardPage = () => {
 
   useEffect(() => {
     if (web3Modal.cachedProvider) {
-      console.log("aa")
       loadWeb3Modal();
     }
   }, [loadWeb3Modal]);
@@ -44,19 +40,43 @@ const DashboardPage = () => {
       const fanToken = new Contract(tokenAddress, abis.fanToken, signer)
       const name = await fanToken.name()
       const symbol = await fanToken.symbol()
+
+      const vesting = new Contract(addresses.Vesting, abis.vesting, signer)
+      const vestingAmount = await vesting.remainingAmount(tokenAddress)
+      const redeemableAmount = await vesting.redeemableAmount(tokenAddress)
+      
+      const staking = new Contract(addresses.Staking, abis.staking, signer)
+      const isStakingPaused = await staking.tokensStakingPaused(tokenAddress)
+
       const tokenInfo = {
         address: tokenAddress,
         name: name,
         symbol: symbol,
-        vestingAmount: 8888888
+        vestingAmount: vestingAmount.toNumber(),
+        redeemableAmount: redeemableAmount.toNumber(),
+        isStakingPaused: isStakingPaused,
       }
       tokenArray.push(tokenInfo)
     }
     setTokensAddress(tokenArray)
   }
 
-  const withdrawToken = () => {
-    console.log("Withdraw Token!!")
+  const withdrawToken = async (address) => {
+    const signer = await provider.getSigner()
+    const vesting = new Contract(addresses.Vesting, abis.vesting, signer)
+    vesting.redeem(address)
+  }
+
+  const restartStaking = async (address) => {
+    const signer = await provider.getSigner()
+    const staking = new Contract(addresses.Staking, abis.staking, signer)
+    staking.resumeStakingOf(address)
+  }
+
+  const stopStaking = async (address) => {
+    const signer = await provider.getSigner()
+    const staking = new Contract(addresses.Staking, abis.staking, signer)
+    staking.pauseStakingOf(address)
   }
 
   return (
@@ -64,11 +84,10 @@ const DashboardPage = () => {
       <Dashboard
         provider={provider} 
         loadWeb3Modal={loadWeb3Modal}
-        path={path}
-        name={name}
-        vestingAmount={vestingAmount}
         tokens={tokens}
         withdrawToken={withdrawToken}
+        restartStaking={restartStaking}
+        stopStaking={stopStaking}
       />
     </div>
   );
