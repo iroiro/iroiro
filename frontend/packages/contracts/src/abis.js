cb5817401a639ee02b05b0316ac1de095de1c314
toshiakitakase <toshi@tart.tokyo>
2020-10-14 13:51:13 +0900

    add address and json

--- frontend/packages/contracts/src/abis.js
+++ frontend/packages/contracts/src/abis.js
@@ -2,12 +2,14 @@ import erc20Abi from "./abis/erc20.json";
 import ownableAbi from "./abis/ownable.json";
 import tokenFactoryAbi from "./abis/TokenFactory.json";
 import fanTokenAbi from "./abis/FanToken.json";
+import stakingAbi from "./abis/Staking.json"
 
 const abis = {
   erc20: erc20Abi,
   ownable: ownableAbi,
   tokenFactory: tokenFactoryAbi,
   fanToken: fanTokenAbi,
+  staking: stakingAbi,
 };
 
 export default abis;
