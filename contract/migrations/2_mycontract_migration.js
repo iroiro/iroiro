3ca3ba3dfe90b6ca2e57cb02bc68a612b2ac85de
Tsukasa Noguchi <wildmouse0813@gmail.com>
2020-10-19 20:30:51 +0900

    Update chaintoken migration for ropsten

--- contract/migrations/2_mycontract_migration.js
+++ contract/migrations/2_mycontract_migration.js
@@ -1,21 +1,32 @@
 const MyContract = artifacts.require('MyContract')
-const { LinkToken } = require('@chainlink/contracts/truffle/v0.4/LinkToken')
-const { Oracle } = require('@chainlink/contracts/truffle/v0.6/Oracle')
+const {LinkToken} = require('@chainlink/contracts/truffle/v0.4/LinkToken')
+const {Oracle} = require('@chainlink/contracts/truffle/v0.6/Oracle')
 
 module.exports = async (deployer, network, [defaultAccount]) => {
   // Local (development) networks need their own deployment of the LINK
   // token and the Oracle contract
-  if (!network.startsWith('live')) {
+  if (network.startsWith("ropsten")) {
+    Oracle.setProvider(deployer.provider)
+    try {
+      await deployer.deploy(Oracle, "0x20fE562d797A42Dcb3399062AE9546cd06f63280", {from: defaultAccount})
+        .then(instance => {
+          // Chainlink node account address
+          instance.setFulfillmentPermission("0xe0d7534B5E0Fa7d77d99Ce681906514323870aF3", true, {from: defaultAccount})
+        })
+      await deployer.deploy(MyContract, "0x20fE562d797A42Dcb3399062AE9546cd06f63280")
+    } catch (err) {
+      console.error(err)
+    }
+  } else if (!network.startsWith('live')) {
     LinkToken.setProvider(deployer.provider)
     Oracle.setProvider(deployer.provider)
     try {
-      await deployer.deploy(LinkToken, { from: defaultAccount })
-      await deployer.deploy(Oracle, LinkToken.address, { from: defaultAccount })
+      await deployer.deploy(LinkToken, {from: defaultAccount})
+      await deployer.deploy(Oracle, LinkToken.address, {from: defaultAccount})
       await deployer.deploy(MyContract, LinkToken.address)
     } catch (err) {
       console.error(err)
     }
-  // TODO Add ropsten case
   } else {
     // For live networks, use the 0 address to allow the ChainlinkRegistry
     // contract automatically retrieve the correct address for you
