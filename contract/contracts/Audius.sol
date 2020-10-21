af8e89e83c71f3535ad01ed1a87de86d73922350
wild-mouse <wildmouse0813@gmail.com>
2020-10-21 10:11:06 +0900

    Format

--- contract/contracts/Audius.sol
+++ contract/contracts/Audius.sol
@@ -24,35 +24,35 @@ contract Audius is AudiusInterface {
     mapping(address => bool) private followerClaimed;
 
     constructor(address _factory) public {
-      tokenFactory = TokenFactory(_factory);
+        tokenFactory = TokenFactory(_factory);
     }
 
-    function remainingAmount(address token) public view returns(uint256) {
+    function remainingAmount(address token) public view returns (uint256) {
         FanToken fanToken = FanToken(token);
         return fanToken.balanceOf(address(this));
     }
-    
+
     // Add Audius list
     function addAudiusList(uint256 id, string memory _followersHash, uint256 _followersNum) external override {
-      address token = tokenFactory.creatorTokenOf(msg.sender, id);
-      require(!registerdTokens[token], "Token is already registered");
-      followersHash[token] = _followersHash;
-      followersNum[token] = _followersNum;
-      registerdTokens[token] = true;
+        address token = tokenFactory.creatorTokenOf(msg.sender, id);
+        require(!registerdTokens[token], "Token is already registered");
+        followersHash[token] = _followersHash;
+        followersNum[token] = _followersNum;
+        registerdTokens[token] = true;
     }
 
     // Get the amount of tokens distributed
     function distributedAmount(address token) public override view returns (uint256) {
-      FanToken fanToken = FanToken(token);
-      uint256 balance = fanToken.balanceOf(address(this));
-      return balance.div(followersNum[token]);
+        FanToken fanToken = FanToken(token);
+        uint256 balance = fanToken.balanceOf(address(this));
+        return balance.div(followersNum[token]);
     }
 
     // Claim tokens
     function claim(address token) external override {
-      // TODO Check Chainlink api
-      
-      FanToken fanToken = FanToken(token);
-      fanToken.transfer(msg.sender, distributedAmount(token));
+        // TODO Check Chainlink api
+
+        FanToken fanToken = FanToken(token);
+        fanToken.transfer(msg.sender, distributedAmount(token));
     }
 }
