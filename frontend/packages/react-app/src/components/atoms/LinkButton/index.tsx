68f7851d2e8e42e970a945859f7009b07e2976b5
wild-mouse <wildmouse0813@gmail.com>
2020-11-03 12:27:14 +0900

    Add stories for LinkButton

--- frontend/packages/react-app/src/components/atoms/LinkButton/index.tsx
+++ frontend/packages/react-app/src/components/atoms/LinkButton/index.tsx
@@ -2,14 +2,14 @@ import React from "react"
 import { Link } from 'react-router-dom'
 import { Button } from "rimble-ui"
 
-interface Props {
+export interface LinkOutlineButtonProps {
     readonly p: number
     readonly path: string
     readonly text: string
     readonly mainColor: string
 }
 
-const LinkOutlineButton = ({path, text, mainColor}: Props) => (
+const LinkOutlineButton = ({path, text, mainColor}: LinkOutlineButtonProps) => (
     <Link to={ path } style={{textDecoration: 'none'}}>
       <Button mainColor={mainColor} mr={2}>{ text }</Button>
     </Link>
