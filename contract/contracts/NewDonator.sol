46c66004d1df3a10074c513599bea7f150d2892c
wild-mouse <wildmouse0813@gmail.com>
2020-11-13 20:33:33 +0900

    Implement Donator

---
+++ contract/contracts/NewDonator.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./NewInterfaces.sol";
+
+contract NewDonator is DonatorInterface {
+    // TODO This function should be restricted for authenticated token owners
+    function setDonatee(address _token) public override {
+        tokenDonateeList[_token] = msg.sender;
+    }
+
+    function donate(address _token, uint256 _amount) public override {
+        address donatee = tokenDonateeList[_token];
+        require(donatee != address(0), "Donatee is not registered yet");
+        ERC20 token = ERC20(_token);
+        token.transferFrom(msg.sender, donatee, _amount);
+
+        // TODO emit event
+    }
+}
