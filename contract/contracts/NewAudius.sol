ac2ab5f9268a6d84e46a4ad2d16c12c4054a2c7e
wild-mouse <wildmouse0813@gmail.com>
2020-11-16 14:03:44 +0900

    Add mock claim logic

--- contract/contracts/NewAudius.sol
+++ contract/contracts/NewAudius.sol
@@ -6,7 +6,7 @@ import "./NewInterfaces.sol";
 contract AudiusFollowersDistributer is DistributerInterface {
     function createCampaign(
         address token,
-        // TODO Update arg name to `tokenSender`
+    // TODO Update arg name to `tokenSender`
         address tokenHolder,
         string memory campaignInfoCid,
         string memory recipientsCid,
@@ -45,6 +45,11 @@ contract AudiusFollowersDistributer is DistributerInterface {
 }
 
 contract AudiusFollowersCampaign is CampaignInterface {
+    uint64 public nextUserId = 1;
+    mapping(address => uint64) userIdList;
+    mapping(address => bool) claimedUserList;
+    mapping(bytes32 => bool) private claimKeyHashList;
+
     constructor(
         address _token,
         string memory _campaignInfoCid,
@@ -61,16 +66,34 @@ contract AudiusFollowersCampaign is CampaignInterface {
         status = Status.Active;
     }
 
-    function isClaimable(address token) public view override returns (bool) {
+    function generateClaimKey(uint64 userId) public pure returns (uint256){
+        return uint256(userId) * 10 + 1; // 1 as true
+    }
+
+    function isClaimable() public view override returns (bool) {
+        uint64 userId = userIdList[msg.sender];
+        require(userId > 0, "User is not registered");
+        if (claimedUserList[msg.sender]) {
+            return false;
+        }
+
         return true;
     }
 
+    // TODO Logic could be changed
     function claim(address token) external override {
+        require(isClaimable(), "Token is not claimable");
+        require(!claimedUserList[msg.sender], "Already claimed");
+
+        claimedUserList[msg.sender] = true;
+        ERC20 erc20 = ERC20(token);
+        token.transfer(msg.sender, claimAmount);
+
         emit Claim(claimAmount);
     }
 
     function fulfill(bytes32 _requestId, bytes32 data) public recordChainlinkFulfillment(_requestId) {
-        // TODO: implement logic
+        claimKeyHashList[data] = true;
     }
 
     function requestCheckingIsClaimable(
@@ -79,7 +102,18 @@ contract AudiusFollowersCampaign is CampaignInterface {
         uint256 fee
     // TODO Add other arguments for actual request
     ) external returns (bytes32 requestId) {
+        uint64 userId;
+        if (userIdList[msg.sender] == 0) {
+            userId = nextUserId;
+            userIdList[msg.sender] = userId;
+            nextUserId = nextUserId.add(1);
+        } else {
+            userId = userIdList[msg.sender];
+        }
+
         Chainlink.Request memory request = buildChainlinkRequest(_jobId, address(this), this.fulfill.selector);
+        request.add("cid", followersHash);
+        request.add("userAddress", userAddress);
 
         return sendChainlinkRequestTo(_oracle, request, fee);
     }
