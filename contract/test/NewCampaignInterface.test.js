e265792e4c59c77145dcfb53e32abb14cca30af5
wild-mouse <wildmouse0813@gmail.com>
2020-11-16 17:25:57 +0900

    Update test with time helper

--- contract/test/NewCampaignInterface.test.js
+++ contract/test/NewCampaignInterface.test.js
@@ -5,8 +5,9 @@ const {assert, expect} = require("chai")
 const Campaign = contract.fromArtifact("CampaignInterface")
 const FanToken = contract.fromArtifact("FanToken")
 
-describe("CampaignInterface", async () => {
+describe("CampaignInterface", () => {
   const [owner, alice, minter] = accounts;
+  let now, future;
 
   beforeEach(async () => {
     this.abctoken = await FanToken.new(
@@ -15,14 +16,16 @@ describe("CampaignInterface", async () => {
     this.xyztoken = await FanToken.new(
         "XYZToken", "XYZ", 1000000000, owner, 5, owner, 50, 5, {from: owner}
     )
+    now = await time.latest()
+    future = now.add(time.duration.weeks(1))
     this.campaign = await Campaign.new(
         this.abctoken.address,
         "campaign info cid",
         "recipients cid",
         100000,
         alice,
-        1000,
-        2000,
+        now,
+        future,
         "https://example.com/",
         { from: owner })
   })
@@ -34,8 +37,8 @@ describe("CampaignInterface", async () => {
     expect((await this.campaign.claimAmount()).toString()).to.be.equal("100000")
     expect((await this.campaign.claimedNum()).toString()).to.be.equal("0")
     expect(await this.campaign.refundDestination()).to.be.equal(alice)
-    expect((await this.campaign.startDate()).toString()).to.be.equal("1000")
-    expect((await this.campaign.endDate()).toString()).to.be.equal("2000")
+    expect(await this.campaign.startDate()).to.be.bignumber.equal(now)
+    expect(await this.campaign.endDate()).to.be.bignumber.equal(future)
     expect(await this.campaign.baseURL()).to.be.equal("https://example.com/")
     expect((await this.campaign.status()).toString()).to.be.equal("0")
   })
