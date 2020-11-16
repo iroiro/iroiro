8c21593e07b6de6154e23a081e52ce51e8cf794d
wild-mouse <wildmouse0813@gmail.com>
2020-11-16 18:48:50 +0900

    Add test skeltons for audius campaign

--- contract/test/AudiusFollowersCampaign.test.js
+++ contract/test/AudiusFollowersCampaign.test.js
@@ -2,27 +2,56 @@ const {accounts, contract} = require("@openzeppelin/test-environment")
 const {constants} = require("@openzeppelin/test-helpers")
 const {assert, expect} = require("chai")
 
-const Distributers = contract.fromArtifact("AudiusFollowersDistributer")
+const Campaign = contract.fromArtifact("AudiusFollowersCampaign")
 const FanToken = contract.fromArtifact("FanToken")
 
-xdescribe("AudiusFollowersDistributer", () => {
-    describe("createCampaign", () => {
-        it("throws an error if msg.sender is not matched to token sender", async () => {
+xdescribe("AudiusFollowersCampaign", () => {
+    describe("generateClaimKey", () => {
+        it("returns claim key", async () => {
 
         })
-        it("throws an error if there is no allowance", async () => {
+    })
+    describe("isClaimable", () => {
+        it("returns false if user is is not registered", async () => {
 
         })
-        it("throws an error if allowance is not enough to recipients", async () => {
+        it("returns false if user already claimed", async () => {
 
         })
-        it("create new campaign", async() => {
+        it("returns false if claim hash is not matched", async () => {
 
         })
-        it("transfers token of approved amount", async () => {
+        it("returns true if claim hash is matched", async () => {
 
         })
-        it("emits event", async() => {
+    })
+    describe("claim", () => {
+        it("throws an error if user is not claimable", async () => {
+
+        })
+        it("throws an error if user already claimed", async () => {
+
+        })
+        it("transfer token to user with specific amount", async () => {
+
+        })
+        it("emits event", async () => {
+
+        })
+    })
+    describe("fulfill", () => {
+        it("set key hash list", async () => {
+
+        })
+    })
+    describe("requestCheckingIsClaimable", async () => {
+        it("set new user id if its not registered yet", async () => {
+
+        })
+        it("does not set new user id if its already registered", async () => {
+
+        })
+        it("returns request id", async () => {
 
         })
     })
