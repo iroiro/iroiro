d67d132b4d5209d2537dd6c9886a6222045c561d
toshiakitakase <toshi@tart.tokyo>
2020-11-25 09:36:27 +0900

    Add routing

--- frontend/packages/react-app/src/App.tsx
+++ frontend/packages/react-app/src/App.tsx
@@ -1,25 +1,49 @@
 import React, { Component, useContext, createContext } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import TopPage from "./components/pages/TopPage";
-import CreateTokenPage from "./components/pages/CreateTokenPage";
 import ExplorePage from "./components/pages/ExplorePage";
 import DashboardPage from "./components/pages/DashboardPage";
 import TokenDetailPage from "./components/pages/TokenDetailPage";
-import WithdrawAudiusPage from "./components/pages/WithdrawAudiusPage";
-import AudiusDisributionPage from "./components/pages/AudiusDisributionPage";
+import ExternalTokenDetailPage from "./components/pages/ExternalTokenDetailPage";
+import SelectDistributersPage from "./components/pages/SelectDistributersPage";
+import NewDistributerPage from "./components/pages/NewDistributerPage";
 
 const App = () => {
   return (
     <div>
       <Router>
         <div>
-          <Route exact path="/" component={TopPage} />
-          <Route path="/create" component={CreateTokenPage} />
-          <Route path="/explore" component={ExplorePage} />
-          <Route path="/dashboard" component={DashboardPage} />
-          <Route path="/token/:address" component={TokenDetailPage} />
-          <Route path="/audius-token" component={WithdrawAudiusPage} />
-          <Route path="/audius/:address" component={AudiusDisributionPage} />
+          <Route exact path="/" component={ExplorePage} />
+
+          {/* For Creator */}
+          <Route exact path="/dashboard" component={DashboardPage} />
+          <Route path="/dashboard/token" component={ExternalTokenDetailPage} />
+          <Route
+            path="/dashboard/distributers"
+            component={SelectDistributersPage}
+          />
+          <Route
+            exact
+            path="/dashboard/distributer/:address"
+            component={NewDistributerPage}
+          />
+          <Route
+            path="/dashboard/distributer/:address/:campaignId"
+            component={NewDistributerPage}
+          />
+
+          {/* For Fan */}
+          <Route exact path="/explore" component={ExplorePage} />
+          <Route exact path="/explore/:address" component={TokenDetailPage} />
+          <Route path="/explore/:address/basic" component={TokenDetailPage} />
+          <Route
+            path="/explore/:address/campaigns"
+            component={TokenDetailPage}
+          />
+          <Route
+            path="/explore/:address/:campaignId"
+            component={TokenDetailPage}
+          />
+          <Route path="/explore/:address/history" component={TokenDetailPage} />
         </div>
       </Router>
     </div>
---
+++ frontend/packages/react-app/src/components/pages/NewDistributerPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const NewDistributerPage = (props: Props) => {
+  return <>NewDistributerPage!!!</>;
+};
+
+export default NewDistributerPage;
