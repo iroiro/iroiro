b2e23aa20547440d7abdf9bf90672bd3a82ee18c
wild-mouse <wildmouse0813@gmail.com>
2020-12-03 14:42:43 +0900

    Add interfaces

--- frontend/packages/react-app/src/interfaces.ts
+++ frontend/packages/react-app/src/interfaces.ts
@@ -1,5 +1,6 @@
 // TODO Update type
 import { Web3Provider } from "@ethersproject/providers";
+import { CampaignInformation } from "./components/molecules/TokenCampaignCard";
 
 export interface Web3Props {
   readonly provider: Web3Provider | undefined;
@@ -39,4 +40,43 @@ export interface TokenBasic {
   readonly symbol: string;
   readonly decimals: number;
   readonly totalSupply: number;
+}
+
+export interface Distributor {
+  // TODO Add fields
+}
+
+export interface Creator {
+  // TODO Add fields
+}
+
+export interface Claim {
+  // TODO Add fields
+}
+
+export interface CheckRequest {
+  // TODO Add fields
+}
+
+export interface Campaign {
+  readonly id: string;
+  readonly distributor: Distributor;
+  readonly token: string;
+  readonly startDate: string;
+  readonly endDate: string;
+  readonly creator: Creator;
+  readonly campaignInfoCid: string;
+  readonly recipientsCid: string;
+  readonly claimAmount: string;
+  readonly claimedNum: number;
+  readonly status: number;
+  readonly claims: Claim[];
+  readonly checkRequests: CheckRequest[];
+}
+
+// TODO Move to page reducer
+export interface TokenInformationState {
+  token?: TokenBasic;
+  campaigns: Campaign[];
+  campaignInformationList: CampaignInformation[];
 }
