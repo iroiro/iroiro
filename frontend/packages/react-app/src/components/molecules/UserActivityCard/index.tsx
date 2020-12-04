e140c6ca31a837ca5d8c79a067390eab8cf47d6b
wild-mouse <wildmouse0813@gmail.com>
2020-12-04 18:11:19 +0900

    Fix activity amount is not displayed

--- frontend/packages/react-app/src/components/molecules/UserActivityCard/index.tsx
+++ frontend/packages/react-app/src/components/molecules/UserActivityCard/index.tsx
@@ -1,10 +1,10 @@
 import * as React from "react";
-import { Card, CardContent, CardHeader, Typography } from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
 import { Activity, TokenBasic } from "../../../interfaces";
 
 export interface UserActivityCardProps {
   readonly activity: Activity;
-  readonly token: TokenBasic;
+  readonly token?: TokenBasic;
 }
 
 const UserActivityCard = ({ activity, token }: UserActivityCardProps) => {
@@ -15,7 +15,7 @@ const UserActivityCard = ({ activity, token }: UserActivityCardProps) => {
           {new Date(parseInt(activity.timestamp) * 1000).toLocaleDateString()}
         </Typography>
         <Typography>
-          {activity.name} ${token.symbol}
+          {activity.name} {activity.amount} ${token?.symbol}
         </Typography>
       </CardContent>
     </Card>
