e299f546b17da0ddabadec7097ebeacc6c2fcdb2
wild-mouse <wildmouse0813@gmail.com>
2020-12-07 14:09:11 +0900

    Fix dom error

--- frontend/packages/react-app/src/components/organisms/TokenInformationTabs/index.tsx
+++ frontend/packages/react-app/src/components/organisms/TokenInformationTabs/index.tsx
@@ -39,11 +39,7 @@ function TabPanel(props: TabPanelProps) {
       aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
+      {value === index && <Box p={3}>{children}</Box>}
     </div>
   );
 }
