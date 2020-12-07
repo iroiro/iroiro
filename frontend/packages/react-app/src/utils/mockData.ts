60d95700933cacbc10a5a38673a7612d76b075b6
wild-mouse <wildmouse0813@gmail.com>
2020-12-07 20:02:42 +0900

    Add eslint to frontend

--- frontend/packages/react-app/src/utils/mockData.ts
+++ frontend/packages/react-app/src/utils/mockData.ts
@@ -30,34 +30,6 @@ export const campaign: CampaignInfo = {
   campaignMetadata: campaignMetadata,
 };
 
-// from: https://web3js.readthedocs.io/en/v1.2.0/web3-eth-contract.html#id41
-const event: EventData = {
-  returnValues: {
-    myIndexedParam: 20,
-    myOtherIndexedParam: "0x123456789...",
-    myNonIndexParam: "My String",
-  },
-  raw: {
-    data:
-      "0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385",
-    topics: [
-      "0xfd43ade1c09fade1c0d57a7af66ab4ead7c2c2eb7b11a91ffdd57a7af66ab4ead7",
-      "0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385",
-    ],
-  },
-  event: "MyEvent",
-  signature:
-    "0xfd43ade1c09fade1c0d57a7af66ab4ead7c2c2eb7b11a91ffdd57a7af66ab4ead7",
-  logIndex: 0,
-  transactionIndex: 0,
-  transactionHash:
-    "0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385",
-  blockHash:
-    "0xfd43ade1c09fade1c0d57a7af66ab4ead7c2c2eb7b11a91ffdd57a7af66ab4ead7",
-  blockNumber: 1234,
-  address: "0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe",
-};
-
 export const tokenInformationState: TokenInformationState = {
   token: {
     tokenAddress: "0xabcd...1234",
