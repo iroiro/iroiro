5ea2eab723aa2f77fff8bd26ffc96bcca5ba9808
toshiakitakase <toshi@tart.tokyo>
2020-12-18 18:27:42 +0900

    delete unused file and rename file name

--- frontend/packages/react-app/src/components/molecules/ApproveTokenForm/index.stories.tsx
+++ frontend/packages/react-app/src/components/molecules/ApproveTokenForm/index.stories.tsx
@@ -1,17 +1,17 @@
 import * as React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { BrowserRouter } from "react-router-dom";
-import DepositTokenForm, { ApproveTokenFormProps } from "./index";
+import ApproveTokenForm, { ApproveTokenFormProps } from "./index";
 import { distributorFormState } from "../../../utils/mockData";
 
 export default {
-  title: "Molecules/DepositTokenForm",
-  component: DepositTokenForm,
+  title: "Molecules/ApproveTokenForm",
+  component: ApproveTokenForm,
 } as Meta;
 
 const Template: Story<ApproveTokenFormProps> = (args) => (
   <BrowserRouter>
-    <DepositTokenForm {...args} />
+    <ApproveTokenForm {...args} />
   </BrowserRouter>
 );
 
