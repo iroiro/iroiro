c11952ff3852a7f582cedfd832cf2da255d41aeb
wild-mouse <wildmouse0813@gmail.com>
2020-12-24 16:21:42 +0900

    Update stories

--- frontend/packages/react-app/src/components/templates/CreateCampaignPageTemaplate/index.stories.tsx
+++ frontend/packages/react-app/src/components/templates/CreateCampaignPageTemaplate/index.stories.tsx
@@ -2,7 +2,11 @@ import * as React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
 import { BrowserRouter } from "react-router-dom";
 import CreateCampaignPageTemaplate, { CampaignInfo } from "./index";
-import { distributionTargets, tokenInfo } from "../../../utils/mockData";
+import {
+  audiusState,
+  distributorFormState,
+  tokenInfo,
+} from "../../../utils/mockData";
 
 export default {
   title: "Templates/CreateCampaignPageTemaplate",
@@ -15,9 +19,32 @@ const Template: Story<CampaignInfo> = (args) => (
   </BrowserRouter>
 );
 
-export const Default = Template.bind({});
-Default.args = {
+export const Step1 = Template.bind({});
+Step1.args = {
+  active: true,
   tokenInfo: tokenInfo,
-  targets: distributionTargets,
-  targetNumber: 1234567890123450,
+  distributorFormState,
+  audiusState,
+};
+
+export const Step2 = Template.bind({});
+Step2.args = {
+  active: true,
+  tokenInfo: tokenInfo,
+  distributorFormState: {
+    ...distributorFormState,
+    step: 2,
+  },
+  audiusState,
+};
+
+export const Step3 = Template.bind({});
+Step3.args = {
+  active: true,
+  tokenInfo: tokenInfo,
+  distributorFormState: {
+    ...distributorFormState,
+    step: 3,
+  },
+  audiusState,
 };
