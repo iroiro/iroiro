727b65dcdea17a5602263e178b6a6d7c467e9c1c
wild-mouse <wildmouse0813@gmail.com>
2021-01-14 16:10:55 +0900

    Add types

--- frontend/packages/react-app/src/types/index.ts
+++ frontend/packages/react-app/src/types/index.ts
@@ -2,11 +2,13 @@
 /* tslint:disable */
 /* eslint-disable */
 export type { CampaignInterface } from "./CampaignInterface";
+export type { CampaignInterfaceV2 } from "./CampaignInterfaceV2";
 export type { CCTWalletCampaign } from "./CCTWalletCampaign";
 export type { CCTWalletDistributor } from "./CCTWalletDistributor";
 export type { ChainlinkClient } from "./ChainlinkClient";
 export type { ChainlinkRequestInterface } from "./ChainlinkRequestInterface";
 export type { DistributorInterface } from "./DistributorInterface";
+export type { DistributorInterfaceV2 } from "./DistributorInterfaceV2";
 export type { Donator } from "./Donator";
 export type { DonatorInterface } from "./DonatorInterface";
 export type { ENSInterface } from "./ENSInterface";
@@ -19,13 +21,17 @@ export type { LinkTokenInterface } from "./LinkTokenInterface";
 export type { Migrations } from "./Migrations";
 export type { Ownable } from "./Ownable";
 export type { PointerInterface } from "./PointerInterface";
+export type { WalletCampaign } from "./WalletCampaign";
+export type { WalletDistributor } from "./WalletDistributor";
 
 export { CampaignInterface__factory } from "./factories/CampaignInterface__factory";
+export { CampaignInterfaceV2__factory } from "./factories/CampaignInterfaceV2__factory";
 export { CCTWalletCampaign__factory } from "./factories/CCTWalletCampaign__factory";
 export { CCTWalletDistributor__factory } from "./factories/CCTWalletDistributor__factory";
 export { ChainlinkClient__factory } from "./factories/ChainlinkClient__factory";
 export { ChainlinkRequestInterface__factory } from "./factories/ChainlinkRequestInterface__factory";
 export { DistributorInterface__factory } from "./factories/DistributorInterface__factory";
+export { DistributorInterfaceV2__factory } from "./factories/DistributorInterfaceV2__factory";
 export { Donator__factory } from "./factories/Donator__factory";
 export { DonatorInterface__factory } from "./factories/DonatorInterface__factory";
 export { ENSInterface__factory } from "./factories/ENSInterface__factory";
@@ -38,3 +44,5 @@ export { LinkTokenInterface__factory } from "./factories/LinkTokenInterface__fac
 export { Migrations__factory } from "./factories/Migrations__factory";
 export { Ownable__factory } from "./factories/Ownable__factory";
 export { PointerInterface__factory } from "./factories/PointerInterface__factory";
+export { WalletCampaign__factory } from "./factories/WalletCampaign__factory";
+export { WalletDistributor__factory } from "./factories/WalletDistributor__factory";
