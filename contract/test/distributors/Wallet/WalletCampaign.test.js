59789e62bbd7737f3a3b909aaaf398882e42a586
wild-mouse <wildmouse0813@gmail.com>
2021-01-14 16:30:21 +0900

    Add failed case to wallet campaign test

--- contract/test/distributors/Wallet/WalletCampaign.test.js
+++ contract/test/distributors/Wallet/WalletCampaign.test.js
@@ -1,5 +1,10 @@
 const { accounts, contract } = require("@openzeppelin/test-environment");
-const { BN, expectEvent, time } = require("@openzeppelin/test-helpers");
+const {
+  BN,
+  expectEvent,
+  expectRevert,
+  time,
+} = require("@openzeppelin/test-helpers");
 const { assert, expect } = require("chai");
 
 const Distributor = contract.fromArtifact("WalletDistributor");
@@ -13,6 +18,15 @@ describe("WalletCampaign", () => {
 
   const merkleRoot =
     "0x33e954d45e481a7c78be8cb27f39277113b2519ef0c0d237ab91a054d4bc4f7a";
+  const proof = [
+    "0xb2edb7e841c03b8394638ba04b3bd2e9769b0d29586a4d476bf71d84e1612b46",
+    "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
+    "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
+    "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
+    "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
+    "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
+    "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
+  ];
   const campaignInfoCid = "campaign info cid";
   const recipientsCid = "recipients cid";
   const recipientsNum = 100;
@@ -84,15 +98,7 @@ describe("WalletCampaign", () => {
             1,
             "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
             new BN(100),
-            [
-              "0xb2edb7e841c03b8394638ba04b3bd2e9769b0d29586a4d476bf71d84e1612b46",
-              "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
-              "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
-              "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
-              "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
-              "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
-              "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
-            ]
+            proof
           );
           assert.fail();
         } catch (error) {
@@ -134,15 +140,7 @@ describe("WalletCampaign", () => {
             1,
             "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
             new BN(100),
-            [
-              "0xb2edb7e841c03b8394638ba04b3bd2e9769b0d29586a4d476bf71d84e1612b46",
-              "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
-              "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
-              "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
-              "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
-              "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
-              "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
-            ]
+            proof
           );
           assert.fail();
         } catch (error) {
@@ -153,20 +151,12 @@ describe("WalletCampaign", () => {
     });
 
     describe("active campaign", () => {
-      it("simplest claim", async () => {
+      it("claim", async () => {
         await cc.claim(
           1,
           "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
           new BN(100),
-          [
-            "0xb2edb7e841c03b8394638ba04b3bd2e9769b0d29586a4d476bf71d84e1612b46",
-            "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
-            "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
-            "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
-            "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
-            "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
-            "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
-          ]
+          proof
         );
       });
 
@@ -175,21 +165,80 @@ describe("WalletCampaign", () => {
           1,
           "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
           new BN(100),
-          [
-            "0xb2edb7e841c03b8394638ba04b3bd2e9769b0d29586a4d476bf71d84e1612b46",
-            "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
-            "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
-            "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
-            "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
-            "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
-            "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
-          ],
+          proof,
           { from: alice }
         );
         expectEvent(receipt, "Claim", {
           from: "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
           to: "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
         });
+      });
+
+      it("revert if index is invalid", async () => {
+        await expectRevert(
+          cc.claim(
+            2,
+            "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
+            new BN(100),
+            proof,
+            { from: alice }
+          ),
+          "MerkleDistributor: Invalid proof."
+        );
+      });
+
+      it("revert if address is invalid", async () => {
+        await expectRevert(
+          cc.claim(
+            1,
+            "0x01dc7f8c928cea27d8ff928363111c291beb20b2",
+            new BN(100),
+            proof,
+            { from: alice }
+          ),
+          "MerkleDistributor: Invalid proof."
+        );
+      });
+
+      it("revert if amount is invalid", async () => {
+        await expectRevert(
+          cc.claim(
+            1,
+            "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
+            new BN(1000),
+            [
+              "0xb2edb7e841c03b8394638ba04b3bd2e9769b0d29586a4d476bf71d84e1612b46",
+              "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
+              "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
+              "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
+              "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
+              "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
+              "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
+            ],
+            { from: alice }
+          ),
+          "MerkleDistributor: Invalid proof."
+        );
+      });
+
+      it("revert if proof is invalid", async () => {
+        await expectRevert(
+          cc.claim(
+            1,
+            "0x01dC7F8C928CeA27D8fF928363111c291bEB20b1",
+            new BN(100),
+            [
+              "0x0f2293d2199b068a92bd8359ac7f189a4ac49c6aaefcfefdbd3b24fae3ffc198",
+              "0x6bea169605062ad96694d33c2918b3a12ffae68cb4a2238921f37e37e2640c0b",
+              "0x356bdf6769a352b886c6f54b3e003a35e0ec7de615121d9544c3bdc5779f457d",
+              "0xf8cb76a9be4588036a88209807d0293ca1a0d7dd100c1bfac881bdb8fa6302c5",
+              "0xb13a9406568e667caa70cc8b271c9ada0ff7b8ce4ebe5e6889e07632db66809e",
+              "0x3f10ffaf7f1fed0a776fe6b06f4e4a0562ea6996baa71ae99a1a78ff5af467dd",
+            ],
+            { from: alice }
+          ),
+          "MerkleDistributor: Invalid proof."
+        );
       });
     });
   });
