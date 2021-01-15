73dc06f3a5f0ccc0c5ac19da2434321e8b77e616
toshiakitakase <toshi@tart.tokyo>
2021-01-15 14:09:54 +0900

    Fix wallet bug

--- frontend/packages/react-app/src/utils/connectors.ts
+++ frontend/packages/react-app/src/utils/connectors.ts
@@ -2,13 +2,11 @@ import { InjectedConnector } from "@web3-react/injected-connector";
 import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 import { AuthereumConnector } from "@web3-react/authereum-connector";
 import { FortmaticConnector } from "@web3-react/fortmatic-connector";
-import { MagicConnector } from "@web3-react/magic-connector";
 import { PortisConnector } from "@web3-react/portis-connector";
 import { TorusConnector } from "@web3-react/torus-connector";
 
 const POLLING_INTERVAL = 12000;
 const RPC_URLS: { [chainId: number]: string } = {
-  1: process.env.REACT_APP_RPC_URL_1 as string,
   4: process.env.REACT_APP_RPC_URL_4 as string,
 };
 
