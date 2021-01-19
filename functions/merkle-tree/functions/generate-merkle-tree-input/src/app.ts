d713d9ba5fb74f393ab9db66381c8bc179788993
wild-mouse <wildmouse0813@gmail.com>
2021-01-19 18:52:34 +0900

    Add uploading input file to s3

--- functions/merkle-tree/functions/generate-merkle-tree-input/src/app.ts
+++ functions/merkle-tree/functions/generate-merkle-tree-input/src/app.ts
@@ -3,7 +3,10 @@ import {
   APIGatewayProxyEvent,
 } from "aws-lambda";
 import { OldFormat } from "@iroiro/merkle-distributor/src/parse-balance-map";
-import web3 from "web3";
+import { isAddress } from "web3-utils";
+import { S3 } from "aws-sdk";
+
+const s3 = new S3();
 
 const ipfsClient = require("ipfs-http-client");
 
@@ -18,10 +21,20 @@ exports.lambdaHandler = async (
   event: APIGatewayProxyEvent,
   context: APIGatewayEventRequestContext
 ) => {
-  // get cid from input
+  // TODO get cid from input
 
   const cid = "QmNvbpz6i5FymwMpoTi2e5HJGmFP6WarKTrpzNzY26BTd5";
-  const targets = await getFile(cid);
+  // TODO activate
+  // const targets = await getFile(cid);
+  const targets = {
+    targets: [
+      "0x4B8619890fa9C3cF11C497961eB4b970D440127F",
+      "0x84d800DaE0Bdb31A4DE9918782bffCc8D041c1b8",
+      "0x9668a1605Be15b66181d6C4cAD20D4c3Ee0DBDb1",
+    ],
+    type: "address",
+  };
+  console.log(targets);
 
   if (targets.type !== "address") {
     // TODO throw error
@@ -48,18 +61,36 @@ exports.lambdaHandler = async (
   console.debug(input);
 
   // Upload merkle tree input to S3
-
-  // Get upload objects ARN
+  const merkleTreeBucket = process.env.INPUT_BUCKET;
+  console.log(merkleTreeBucket);
+  // TODO Get merkle tree cid
+  const merkleTreeInputKey = "input.json";
+  // Upload merkle tree to S3
+  const putObjectParams = {
+    Bucket: merkleTreeBucket,
+    Key: merkleTreeInputKey,
+    Body: JSON.stringify(input),
+  };
+  await s3
+    .putObject(putObjectParams)
+    .promise()
+    .then((result) => {
+      console.info(result);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 
   return {
-    // return object ARN
+    bucket: merkleTreeBucket,
+    key: merkleTreeInputKey,
   };
 };
 
 const isAllAddress = (targets: Targets): boolean => {
   return (
     targets.targets.find((address) => {
-      return !web3.utils.isAddress(address);
+      return !isAddress(address);
     }) === undefined
   );
 };
