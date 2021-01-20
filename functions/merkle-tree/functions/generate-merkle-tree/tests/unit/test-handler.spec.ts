775bcb666f7545c2ce54eda269149af1622035a4
wild-mouse <wildmouse0813@gmail.com>
2021-01-20 15:26:37 +0900

    Add unit tests for generator

--- functions/merkle-tree/functions/generate-merkle-tree/tests/unit/test-handler.spec.ts
+++ functions/merkle-tree/functions/generate-merkle-tree/tests/unit/test-handler.spec.ts
@@ -10,13 +10,21 @@ const chai = require("chai");
 const expect = chai.expect;
 let event: APIGatewayProxyEvent, context: APIGatewayEventRequestContext;
 
-describe("Tests Stock Checker", function () {
+describe("Tests Generator", function () {
   it("Verifies response", async () => {
+    event = {
+      object: process.env.INPUT_BUCKET,
+      // TODO update key
+      key: "input.json",
+    };
     const result = await app.lambdaHandler(event, context);
+    console.debug("result", result);
 
     expect(result).to.be.an("object");
-    expect(result.stock_price).to.be.an("number");
-    expect(result.stock_price).to.be.at.least(0);
-    expect(result.stock_price).to.be.at.most(100);
+    expect(result.bucket).to.be.an("string");
+    expect(result.bucket).to.equal(process.env.MERKLE_TREE_BUCKET);
+    expect(result.key).to.be.an("string");
+    // TODO update
+    expect(result.key).to.equal("testcid.json");
   });
 });
