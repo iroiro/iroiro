1729519158960d2840a80601054b914b7c5973bd
wild-mouse <wildmouse0813@gmail.com>
2021-03-05 21:01:37 +0900

    Update hardhat config

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -4,4 +4,12 @@ require("hardhat-gas-reporter");
 
 module.exports = {
   solidity: "0.7.6",
+  gasReporter: {
+    excludeContracts: [
+      "Migrations",
+      "DistributorInterfaceV1",
+      "ERC20",
+      "ERC20Mock",
+    ],
+  },
 };
