09299aca762422e241ba6a5dbb795a941c8e6a64
wild-mouse <wildmouse0813@gmail.com>
2021-03-19 21:05:02 +0900

    Fix paths

--- frontend/packages/react-app/src/components/templates/DashboardPageTemplate/index.tsx
+++ frontend/packages/react-app/src/components/templates/DashboardPageTemplate/index.tsx
@@ -67,7 +67,7 @@ const DashboardPageTemplate: React.FC<DashboardPageTemplateProps> = ({
               color="creator"
               onClick={() =>
                 history.push(
-                  `/dashboard/distributors/${walletDistributor.id}/${walletDistributor.type}`
+                  `/dashboard/token/distributors/${walletDistributor.id}/${walletDistributor.type}`
                 )
               }
             />
@@ -80,7 +80,7 @@ const DashboardPageTemplate: React.FC<DashboardPageTemplateProps> = ({
               color="creator"
               onClick={() =>
                 history.push(
-                  `/dashboard/distributors/${urlDistributor.id}/${urlDistributor.type}`
+                  `/dashboard/token/distributors/${urlDistributor.id}/${urlDistributor.type}`
                 )
               }
             />
@@ -95,7 +95,7 @@ const DashboardPageTemplate: React.FC<DashboardPageTemplateProps> = ({
               color="creator"
               onClick={() =>
                 history.push(
-                  `/dashboard/distributors/${emailDistributor.id}/${emailDistributor.type}`
+                  `/dashboard/token/distributors/${emailDistributor.id}/${emailDistributor.type}`
                 )
               }
             />
